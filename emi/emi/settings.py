277c5b24e620e2947ef697e56ed414b768bc345a
marty-petra <martynas.cloud@gmail.com>
2025-07-18 08:15:48 +0300

    Add files via upload

---
+++ emi/emi/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'dummy-key-for-testing'
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+ROOT_URLCONF = 'emi.urls'
+MIDDLEWARE = []
+INSTALLED_APPS = []
+WSGI_APPLICATION = 'emi.wsgi.application'
