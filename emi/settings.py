06057d0988361b097874ccf91e6ab4889f287f2b
marty-petra <martynas.cloud@gmail.com>
2025-07-18 22:31:22 +0300

    Add files via upload

--- emi/settings.py
+++ emi/settings.py
@@ -1,7 +1,56 @@
-SECRET_KEY = 'dummy-key-for-testing'
-DEBUG = False
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'dev-secret-key'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 ROOT_URLCONF = 'emi.urls'
-MIDDLEWARE = []
-INSTALLED_APPS = []
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'emi', 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'emi.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'emi', 'static')]
--- emi/urls.py
+++ emi/urls.py
@@ -1,6 +1,16 @@
+
+from django.contrib import admin
 from django.urls import path
-from .views import hello_view
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 urlpatterns = [
-    path('', hello_view),
+    path('admin/', admin.site.urls),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('dashboard/', login_required(dashboard), name='dashboard'),
 ]
