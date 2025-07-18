92fd6002b8d2c48d2c96975ed6c31d1f50639e34
marty-petra <martynas.cloud@gmail.com>
2025-07-18 09:53:38 +0300

    Delete emi directory

--- emi/emi/settings.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'dummy-key-for-testing'
-DEBUG = False
-ALLOWED_HOSTS = ['*']
-ROOT_URLCONF = 'emi.urls'
-MIDDLEWARE = []
-INSTALLED_APPS = []
-WSGI_APPLICATION = 'emi.wsgi.application'
