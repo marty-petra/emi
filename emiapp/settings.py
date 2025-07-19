12ff5720b2953f2854e7ed48d264ffd265a1f379
marty-petra <martynas.cloud@gmail.com>
2025-07-19 13:57:42 +0300

    Delete emiapp directory

--- emiapp/settings.py
+++
@@ -1,2 +0,0 @@
-SECRET_KEY='dummy'
-INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles']
